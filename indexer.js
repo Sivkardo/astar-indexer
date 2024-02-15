82e9188393cab5e46db779176b2aca9dce57e32f
leonard <leonard.pacandi5@gmail.com>
2024-02-15 13:40:07 +0100

    Added basic drawing template

--- indexer.js
+++ indexer.js
@@ -1,15 +1,14 @@
 
 const { ApiPromise, WsProvider } = require('@polkadot/api')
-
 const fs = require("fs");
 const yargs = require("yargs");
+const can = require("canvas");
 
 const filename = "output.txt";
+const documentName = "graph.html"
 const decimals = 10e17;
 
 
-// TODO
-// ##### ADD YARGS #### 
 
 /**
  * TODO
@@ -33,8 +32,6 @@ async function connectApi(endpoint) {
  */
 async function fetchIssuancePerBlock(args) {
 
-  console.info(`Inside fetch`);
-
   const api = await connectApi(args.endpoint);
   var currentBlockNum = parseInt(args.beginBlockNum);
   var blockIssuanceDic = {};
@@ -53,9 +50,11 @@ async function fetchIssuancePerBlock(args) {
     // Fetches the issuance of current block
     const currentBlockIssuance = Math.floor((await apiAtCurrentBlock.query.balances.totalIssuance()) / decimals);
 
+    /*
     console.info(`Current block number: ${currentBlockNum}`);
     console.info(`Current block hash: ${currentBlockHash}`);
     console.info(`Issuance of current block: ${currentBlockIssuance}\n`);
+    */
 
     blockIssuanceDic[currentBlockNum] = currentBlockIssuance;
 
@@ -69,52 +68,108 @@ async function fetchIssuancePerBlock(args) {
 }
 
 /**
- * 
+ * TODO
+ */
+function createGraphHTML(data) {
+
+  // TODO
+  // everything
+  const canvas = can.createCanvas(1200, 800);
+  const context = canvas.getContext('2d');
+
+  const GRAPH_TOP = 60;
+  const GRAPH_BOTTOM = 740;
+  const GRAPH_LEFT = 60;
+  const GRAPH_RIGHT = 1240;
+
+  const GRAPH_HEIGHT = 680;
+  const GRAPH_WIDTH = 1180;
+
+  // Draw X and Y axis
+  context.beginPath();
+  context.moveTo(GRAPH_LEFT, GRAPH_TOP);
+  context.lineTo(GRAPH_LEFT, GRAPH_BOTTOM);
+  context.lineTo(GRAPH_RIGHT, GRAPH_BOTTOM);
+  context.stroke();
+
+  
+
+
+  // Serialize the canvas to a data URL
+  const dataUrl = canvas.toDataURL();
+
+  const contents = `
+    <!DOCTYPE html>
+    <html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Astar Issuance Graph</title>
+    </head>
+    <body>
+        <h1>Graph yes yes!</h1>
+        <img src="${dataUrl}" alt="Canvas Image">
+    </body>
+    </html>
+  `;
+
+  fs.writeFileSync(documentName, contents);
+}
+
+/**
+ * TODO
  */
 async function drawGraph(args) {
-
-    console.log(args)
-
-    console.info(`Inside drawGraph`);
 
     // TODO
     // error handling
     await fetchIssuancePerBlock(args);
     const data = JSON.parse(fs.readFileSync(filename));
 
-    console.log(data);
+    console.log(data)
+    
+    /*
+    for (const [num, issuance] of Object.entries(data)) {
+      console.log(`Block number: ${num}   Issuance: ${issuance}`);
+    }
+    */
 
+    createGraphHTML();
+
+    // TODO
+    // open HTML file
 }
 
 
 async function main() {
 
-    //TODO
-    // make drawGraph the default function
-    // add descriptions
     await yargs
       .options({
         endpoint: {
-            alias: 'e',
-            default: 'wss://rpc.astar.network',
+            alias: "e",
+            default: "wss://rpc.astar.network",
+            description: "WSS endpoint",
             string: true,
             global: true
         },
         beginBlockNum: {
-            alias: 'beg',
-            default: '4932602',
+            alias: "beg",
+            default: "4932602",
+            description: "Number of the first block",
             string: false,
             gobal: true
         },
         endBlockNum: {
-          alias: 'end',
-          default: '5119443',
+          alias: "end",
+          default: "5119443",
+          description: "Number of the last block",
           string: false,
           gobal: true
         },
         stride: {
-          alias: 's',
-          default: '1000',
+          alias: "s",
+          default: "1000",
+          description: "Size of stride between checked blocks",
           string: false,
           gobal: true
         }
@@ -126,17 +181,6 @@ async function main() {
         drawGraph
       )
       .parse();
-
-      console.info(`Inside main`);
-
-      drawGraph(yargs);
-    /*
-    const endpoint = 'wss://rpc.astar.network';
-    const api = await connectApi(endpoint);
-
-    await fetchIssuancePerBlock(api, 4933602, 4947444, 800);
-    drawGraph(filename);
-    */
 
 };
 
