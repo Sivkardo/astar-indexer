2d4b0fb31ae8a0e74d949bc08e983793be8e1205
leonard <leonard.pacandi5@gmail.com>
2024-03-01 17:11:44 +0100

    Minor additions and fixes

--- indexer.js
+++ indexer.js
@@ -10,11 +10,11 @@ const documentName = "graph.html"
 const decimals = 10e17;
 
 
-const keyBlockNumbers = [ 
-                          4932602, // New Fee System
-                          5119443, // Hybrid Inflation Model
-                          5514934  // Tokenomics 2.0
-                        ];
+const keyBlockData = {
+  4932602: "New Fee System",
+  5119443: "Hybrid Inflation",
+  5514934: "Tokenomics 2.0"
+};
 
 
 /**
@@ -50,10 +50,10 @@ async function fetchIssuancePerBlock(args) {
   const str = parseInt(args.stride);
 
   // Check if user inputed proper block number interval range
-  if(startBlockNum >= end) {
+  if (startBlockNum >= end) {
     throw new Error("Starting block number must be smaller than ending block number!");
   }
-  
+
   var blockIssuanceDic = {};
 
   // Fetch all block hashes
@@ -64,7 +64,7 @@ async function fetchIssuancePerBlock(args) {
     blockHashesPromises.push(api.rpc.chain.getBlockHash(i));
   }
   // Make sure the ending block is included (in case ending block is skipped due to stride)
-  if (i < end + str) {
+  if (i > end && i != end + str) {
     blockHashesPromises.push(api.rpc.chain.getBlockHash(end));
   }
   const blockHashes = await Promise.all(blockHashesPromises);
@@ -90,11 +90,14 @@ async function fetchIssuancePerBlock(args) {
   }));
 
 
-  for (var i = 0; i < currentBlockIssuance.length; i++) {
+  for (var i = 0; i < currentBlockIssuance.length - 1; i++) {
     blockIssuanceDic[startBlockNum + (i * str)] = currentBlockIssuance[i];
   }
 
+  blockIssuanceDic[end] = currentBlockIssuance[currentBlockIssuance.length - 1];
+
   var keyBlockPoints = [];
+  var keyBlockNumbers = Object.keys(keyBlockData);
   keyBlockPoints.push({ x: `${startBlockNum}`, y: currentBlockIssuance[0] });
   for (var i = 0; i < keyBlockNumbers.length; i++) {
     if (keyBlockNumbers[i] < startBlockNum || keyBlockNumbers[i] > end) break;
@@ -107,7 +110,7 @@ async function fetchIssuancePerBlock(args) {
     blockIssuanceDic[parseInt(keyBlockNumbers[i])] = issuanceAtKeyBlock;
   }
 
-  keyBlockPoints.push({ x: `${startBlockNum + str * (blockHashes.length - 1)}`, y: currentBlockIssuance[currentBlockIssuance.length - 1] });
+  keyBlockPoints.push({ x: `${end}`, y: currentBlockIssuance[currentBlockIssuance.length - 1] });
 
   fs.writeFileSync(filename, JSON.stringify(blockIssuanceDic));
 
@@ -193,6 +196,18 @@ function createGraphHTML(data, keyPointData) {
     },
   };
 
+  var text = "\"";
+  for (var i = 2; i < keyPointData.length; i++) {
+    var x1 = parseInt(keyPointData[i - 1].x);
+    var y1 = parseInt(keyPointData[i - 1].y);
+    var x2 = parseInt(keyPointData[i].x);
+    var y2 = parseInt(keyPointData[i].y);
+
+    var slope = Math.round(((y2 - y1) / (x2 - x1)) * 10) / 100;
+
+    text = text.concat(`${keyBlockData[x1]}: ${x1} to ${x2} → ${slope}% grade<br><br> `);
+  }
+  text = text.concat("\"");
 
   const contents = `
     <!DOCTYPE html>
@@ -206,24 +221,29 @@ function createGraphHTML(data, keyPointData) {
           body, html {
             margin: 0 auto;
             padding: 0 auto;
-            width: 95%;
-            height: 95%;
+            width: 100%;
+            height: 100%;
+            text-align: center;
           }
-          canvas {
+          div {
             display: block;
             margin: 0 auto;
-            width: 95vw;
-            height: 95vw;
+            width: 80vw;
+            height: 80vw;
           }
         </style>
     </head>
     <body>
         <div>
           <canvas id="myChart"></canvas>
+          <p id="slopeText"></p>
         </div>
         <script>
             var ctx = document.getElementById('myChart').getContext('2d');
             var myChart = new Chart(ctx, ${JSON.stringify(chartConfig)});
+
+            document.getElementById('slopeText').innerHTML = ${text};
+
         </script>
     </body>
     </html>
@@ -232,6 +252,21 @@ function createGraphHTML(data, keyPointData) {
   fs.writeFileSync(documentName, contents);
 }
 
+/*
+          var text = "";
+            for(var i = 1; i < keyPointData.length - 1; i++) {
+                var x1 = keyPoinData[i - 1].x;
+                var y1 = keyPoinData[i - 1].y;
+                var x2 = keyPoinData[i].x;
+                var y2 = keyPoinData[i].y;
+
+                var slope = ((y2 - y1) / (x2 - x1)) * 100;
+                text = text.concat(x1).concat(" to ").concat(x2).concat(" → ").concat(slope).concat("%\n");
+            }
+
+            document.getElementById('slopeText').innerHTML = text;
+*/
+
 /**
  * Calls functions for fetching issuance data and drawing a graph
  */
@@ -239,6 +274,10 @@ async function drawGraph(args) {
 
   [_, keyPoints] = await fetchIssuancePerBlock(args);
   const data = JSON.parse(fs.readFileSync(filename));
+
+  console.log(data);
+
+  console.log(keyPoints);
 
   createGraphHTML(data, keyPoints);
 }
